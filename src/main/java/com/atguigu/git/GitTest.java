5e7b74357fc26efa4ccf6029f49c6e2581c9a9d3
Xah <773208018@qq.com>
2022-03-12 15:51:59 +0800

    first Commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Siwang Xu
+ * @create 2022/3/12-15:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
